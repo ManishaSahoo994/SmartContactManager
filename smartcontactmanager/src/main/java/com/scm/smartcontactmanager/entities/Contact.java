337b823c6d9367358300d4a1420c3359f9fef212
Manisha Sahoo <manishasahoo2343@gmail.com>
2024-12-18 16:47:21 +0530

    Added entities for the smart contact manager project

---
+++ smartcontactmanager/src/main/java/com/scm/smartcontactmanager/entities/Contact.java
@@ -0,0 +1,38 @@
+package com.scm.smartcontactmanager.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Contact {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String picture;
+    @Lob
+    private String description;
+    private boolean favourite=false;
+
+
+    private String websiteLink;
+    private String linkedInLink;
+
+    //private List<String> sociallinks=new ArrayList<>();
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<SocialLink> links=new ArrayList<>();
+
+}
